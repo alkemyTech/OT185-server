bd6ae6d33c459789550d3fbf4a5f26e39883a566
David Sanchez <tapi.cr7@gmail.com>
2022-04-17 02:02:20 -0300

    added organization model and repository

---
+++ src/main/java/com/alkemy/ong/domain/model/Organization.java
@@ -0,0 +1,77 @@
+package com.alkemy.ong.domain.model;
+
+import com.alkemy.ong.domain.model.audit.Audit;
+import com.alkemy.ong.domain.model.audit.AuditListener;
+import com.alkemy.ong.domain.model.audit.Auditable;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.Where;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+
+@Data
+@ToString
+@NoArgsConstructor
+@Entity(name = "organizations")
+@Where(clause = "is_active=true")
+@SQLDelete(sql = "UPDATE organization SET is_active=false WHERE organization_id=?")
+@EntityListeners(AuditListener.class)
+public class Organization implements Auditable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "organization_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private Integer phone;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "welcomeText", nullable = false )
+    @Type(type="text")
+    private String welcomeText;
+
+    @Column(name = "aboutUsText")
+    @Type(type="text")
+    private String aboutUsText;
+
+    @Column(name = "start_date", nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endDate;
+
+    @Embedded
+    private Audit audit;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Organization organization = (Organization) o;
+        return Objects.equals(id, organization.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/resources/db/migration/v1.OT185-15__table_organizations.sql
@@ -0,0 +1,19 @@
+drop table if exists organizations;
+
+create table organizations
+(
+    organization_id bigint  not null auto_increment,
+    created_at datetime(6)  not null,
+    is_active  bit          not null,
+    updated_at datetime(6)  null,
+    end_date   datetime(6)  null,
+    name       varchar(255) not null,
+    image      varchar(255) not null,
+    address    varchar(255) null,
+    phone           INTEGER  null,
+    email      varchar(255) not null,
+    welcome_text text        not null,
+    about_us_text text        null,
+    start_date datetime(6)  not null,
+    primary key (organization_id)
+) engine = InnoDB;
