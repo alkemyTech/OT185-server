1f3814017998a0f2c0578b8c4b34c01c568ed045
Matias <maticritam@gmail.com>
2022-04-18 14:02:45 -0500

    Se crearon el JwtUtil y JwtRequestFilter

---
+++ src/main/java/com/alkemy/ong/common/auth/filter/JwtRequestFilter.java
@@ -0,0 +1,59 @@
+package com.alkemy.ong.common.auth.filter;
+
+import com.alkemy.ong.common.auth.utils.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Component
+@AllArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    private final UserDetailsCustomService userDetailsCustomService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UserDetails userDetails = userDetailsCustomService.loadUserByUsername(username);
+
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+
+                UsernamePasswordAuthenticationToken authReq
+                        = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, null);
+
+                SecurityContextHolder.getContext().setAuthentication(authReq);
+
+            }
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
