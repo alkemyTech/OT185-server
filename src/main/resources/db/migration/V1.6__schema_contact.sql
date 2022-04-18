ae2d4fc6c7d0e806263c80dc77663323af10b75f
Matias <maticritam@gmail.com>
2022-04-18 12:27:07 -0500

    Se agrego la auditoria a la tabla contactos

--- src/main/resources/db/migration/V1.6__schema_contact.sql
+++ src/main/resources/db/migration/V1.6__schema_contact.sql
@@ -4,6 +4,9 @@ drop table if exists contacts;
 create table contacts
 (
     id_contact bigint     not null auto_increment,
+    created_at datetime(6)  not null,
+    is_active  bit          not null,
+    updated_at datetime(6)  null,
     name    varchar (100) not null,
     phone   varchar (100) not null,
     email   varchar (100) not null,
