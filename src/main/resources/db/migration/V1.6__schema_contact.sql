636dc453357cb7b4d7e2f53a49ebd3d373a471d0
Matias <maticritam@gmail.com>
2022-04-18 09:24:25 -0500

    modificacion de v1.6

--- src/main/resources/db/migration/V1.6__schema_contact.sql
+++ src/main/resources/db/migration/V1.6__schema_contact.sql
@@ -3,13 +3,11 @@ drop table if exists contacts;
 
 create table contacts
 (
---  Fields
-    id_contact bigint  not null auto_increment,
+    id_contact bigint     not null auto_increment,
     name    varchar (100) not null,
     phone   varchar (100) not null,
     email   varchar (100) not null,
     message varchar (255) not null,
---  SoftDelete
     deleted_at bit not null,
 
      primary key (id_contact)
