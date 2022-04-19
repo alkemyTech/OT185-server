41c662dd150b47d5ea97f8e2c58bc1ec3f99c0eb
joelR <rgzjoel14@gmail.com>
2022-04-19 18:31:47 -0300

    Activity model and Repo

---
+++ src/main/java/com/alkemy/ong/domain/model/Activity.java
@@ -0,0 +1,56 @@
+package com.alkemy.ong.domain.model;
+
+import com.alkemy.ong.domain.model.audit.Audit;
+import com.alkemy.ong.domain.model.audit.AuditListener;
+import com.alkemy.ong.domain.model.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "activity")
+@Where(clause = "is_active=true")
+@SQLDelete(sql = "UPDATE alkymer SET is_active=false WHERE alkymer_id=?")
+@EntityListeners(AuditListener.class)
+public class Activity implements Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "activity_id")
+    Long id;
+
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @Column(name = "content", nullable = false)
+    String content;
+
+    @Column(name = "image", nullable = false)
+    String image;
+
+    @Embedded
+    private Audit audit;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Activity activity = (Activity) o;
+        return Objects.equals(id, activity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, content, image);
+    }
+}
