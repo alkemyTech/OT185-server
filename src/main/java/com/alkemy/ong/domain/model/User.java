1338428f019bad2069f8cec4735e69f955c0b5ac
Aizuk <nickbrandariz@gmail.com>
2022-04-19 10:34:35 -0300

    user entity added

---
+++ src/main/java/com/alkemy/ong/domain/model/User.java
@@ -0,0 +1,75 @@
+package com.alkemy.ong.domain.model;
+
+import com.alkemy.ong.domain.model.audit.Audit;
+import com.alkemy.ong.domain.model.audit.AuditListener;
+import com.alkemy.ong.domain.model.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="user")
+@Where(clause = "is_active=true")
+@SQLDelete(sql = "UPDATE user SET is_active=false WHERE user_id=?")
+@EntityListeners(AuditListener.class)
+public class User implements Auditable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, updatable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, updatable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, updatable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false, updatable = false)
+    private String password;
+
+    @Column(name = "photo", nullable = false, updatable = false)
+    private String photo;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Column(name = "role_id")
+    private Long role_id;
+
+
+
+    @Embedded
+    private Audit audit;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
