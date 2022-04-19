acb3b282322a80802424f268be0cc44aa6962483
Ignacio Nicolas Brandariz <nickbrandariz@gmail.com>
2022-04-19 05:49:46 -0300

    [OT185-16] Base Role model and repository creation (#7)
    * RoleEntity created
    
    * refactor RoleEntity deleting audits & softdelete as they are not asked
    
    * sql script for Roles added
    
    * Role Repository added
    
    * Small fixes in Role entity and table
    
    * Added field 'created_at' in both role entity and sql script
    
    * Fixed dependency on RoleRepository

---
+++ src/main/java/com/alkemy/ong/domain/model/Role.java
@@ -0,0 +1,47 @@
+package com.alkemy.ong.domain.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="role_id")
+    private Long id;
+
+    @Column(name="name", nullable = false, updatable = false)
+    private String name;
+
+    @Column(name="description", nullable = false, updatable = false)
+    private String description;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
