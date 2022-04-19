e50915b4bd6ccb4470f4230475e387bee95bbeb4
Aizuk <nickbrandariz@gmail.com>
2022-04-19 15:53:10 -0300

    some minor fix

--- src/main/resources/db/migration/V1.17__table_users.sql
+++ src/main/resources/db/migration/V1.17__table_users.sql
@@ -3,15 +3,17 @@ drop table if exists user;
 create table user
 (
     user_id     bigint  not null auto_increment,
-    first_Name  varchar(255) not null,
+    first_name  varchar(255) not null,
     last_name   varchar(255) not null,
     email       varchar(255) not null,
     password    varchar(255) not null,
     photo       varchar(255) not null,
     created_at  datetime(6) not null,
+    updated_at  datetime(6) null,
+    is_active   bit          not null,
     role_id     bigint  not null,
     primary key (user_id),
-    key role_id (role_id)
-    constraint fk_role
+    key role_id (role_id),
+        constraint fk_role
         foreign key (role_id) references role (role_id),
 ) engine = InnoDB;
