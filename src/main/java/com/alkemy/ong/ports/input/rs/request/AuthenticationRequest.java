44158fe06ffb9e90138536731143e0a63bf373af
joelR <rgzjoel14@gmail.com>
2022-04-20 09:35:57 -0300

    deleted space blank

--- src/main/java/com/alkemy/ong/ports/input/rs/request/AuthenticationRequest.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/request/AuthenticationRequest.java
@@ -10,10 +10,10 @@ import javax.validation.constraints.Size;
 @Getter
 @AllArgsConstructor
 public class AuthenticationRequest {
+
     @Email(regexp = "([a-z0-9]+(\\.?[a-z0-9])*)+@(([a-z]+)\\.([a-z]+))+")
     @NotBlank(message ="The email must not be empty")
     private String email;
-
 
     @Size(min = 4, max = 20, message = "The password must not be empty" )
     private String password;
