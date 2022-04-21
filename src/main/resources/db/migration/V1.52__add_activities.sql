20cf652fc5943d32699387574158de0bad3f64f3
joelR <rgzjoel14@gmail.com>
2022-04-21 19:15:32 -0300

    template insets

---
+++ src/main/resources/db/migration/V1.52__add_activities.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO activity (activity_id, name, content, image, is_active, created_at)
+VALUES               (1,'prueba', '1', '1'   ,true  , current_timestamp()),
+                     (2,'prueba2', '2', '2'   ,true  , current_timestamp());
