2bc0103f61d6eca2e3b757ceb4dd5fc7191714b7
Matías Romero <58831680+ItamMati@users.noreply.github.com>
2022-04-21 07:44:25 -0500

    [OT185-53] Added contactEntity, contactRepository and script of contact (#8)
    * Added model and repository contact
    
    * Added contactEntity and table_contact
    
    * Added contactRepository
    
    * modificacion de v1.6
    
    * Se agrego la auditoria a la tabla contactos
    
    * Se modifica la tabla contacto de plural a singular
    
    * Se mofidica el nombre de la variable softDelete
    
    * Correcciones del pushRequest
    
    * actualizando nombres de las variables

---
+++ src/main/java/com/alkemy/ong/domain/model/Contact.java
@@ -0,0 +1,61 @@
+package com.alkemy.ong.domain.model;
+
+import com.alkemy.ong.domain.model.audit.Audit;
+import com.alkemy.ong.domain.model.audit.AuditListener;
+import com.alkemy.ong.domain.model.audit.Auditable;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "contact")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE contact SET is_active = true WHERE contact_id=?")
+@Where(clause = "is_active = false")
+@EntityListeners(AuditListener.class)
+public class Contact implements Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_id")
+    private Long id;
+
+    @Column(name= "name", nullable = false, updatable = false)
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    private String message;
+
+
+    @Embedded
+    private Audit audit;
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact that = (Contact) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
