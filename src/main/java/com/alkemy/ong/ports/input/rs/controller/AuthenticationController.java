bc07ab921f01047ad2aa9d6addc89909aa6be8b0
joelR <rgzjoel14@gmail.com>
2022-04-22 10:58:47 -0300

    RequestMapping(auth)

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/AuthenticationController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/AuthenticationController.java
@@ -10,10 +10,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-
+@RequestMapping("auth")
 @RestController
 @RequiredArgsConstructor
 public class AuthenticationController {
