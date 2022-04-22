afddbc429b78e08e3e3b46e089812c18129a844c
Aizuk <nickbrandariz@gmail.com>
2022-04-22 20:40:33 -0300

    @Repository annotation added in TestimonialRepository

--- src/main/java/com/alkemy/ong/domain/repository/TestimonialRepository.java
+++ src/main/java/com/alkemy/ong/domain/repository/TestimonialRepository.java
@@ -2,6 +2,8 @@ package com.alkemy.ong.domain.repository;
 
 import com.alkemy.ong.domain.model.Testimonial;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TestimonialRepository extends PagingAndSortingRepository<Testimonial, Long> {
 }
