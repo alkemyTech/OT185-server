9318a7ebbc56447ee2f4dec2bbc9b2b74dc23500
Matias <maticritam@gmail.com>
2022-04-21 22:17:15 -0500

    actualizando auth

--- src/main/java/com/alkemy/ong/common/security/services/UserDetailsCustomService.java
+++ src/main/java/com/alkemy/ong/common/security/services/UserDetailsCustomService.java
@@ -1,28 +1,71 @@
 package com.alkemy.ong.common.security.services;
 
+import com.alkemy.ong.domain.model.Role;
+import com.alkemy.ong.domain.model.User;
+import com.alkemy.ong.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class UserDetailsCustomService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        Optional<User> user = userRepository.findByUsername(username);
+        Optional<User> user = userRepository.findByUsername(username);
+
+        if (user.isEmpty()) {
+
+            throw new UsernameNotFoundException("username or password not fount");
+
+        }
+
+        return new org.springframework.security.core.userdetails.User
+                (user.get().getUsername(), user.get().getPassword(), true, true, true,
+                        true, new ArrayList<>());
+
+    }
+
+
+
+//    private Collection<? extends GrantedAuthority> getAuthorities(
+//            Collection<Role> roles) {
 //
-//        if (user.isEmpty()) {
-//
-//            throw new UsernameNotFoundException("username or password not fount");
-//
-//        }
-//
-//        return new org.springframework.security.core.userdetails.User
-//                (user.get().getUsername(), user.get().getPassword(), true, true, true, true, getAuthorities
-//                        (user.get().getRoles()));
-//    }
+//        return getGrantedAuthorities(new ArrayList<>());
 //    }
 
-        return null;
+//    private List<String> getPrivileges(Collection<Role> roles) {
+//
+//        List<String> privileges = new ArrayList<>();
+//        List<Privilege> collection = new ArrayList<>();
+//        for (Role role : roles) {
+//            privileges.add(role.getName());
+//            collection.addAll(role.getPrivileges());
+//        }
+//        for (Privilege item : collection) {
+//            privileges.add(item.getName());
+//        }
+//        return privileges;
+//    }
+
+//    private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
+//        List<GrantedAuthority> authorities = new ArrayList<>();
+//        for (String privilege : privileges) {
+//            authorities.add(new SimpleGrantedAuthority(privilege));
+//        }
+//        return authorities;
+//    }
     }
-}
+
--- src/main/java/com/alkemy/ong/domain/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/domain/repository/UserRepository.java
@@ -4,6 +4,12 @@ import com.alkemy.ong.domain.model.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+
 }
