e8004f1088c4e9916fdf1d9d10de98b1753877cc
joelR <rgzjoel14@gmail.com>
2022-04-22 10:46:30 -0300

    change UserServiceImpl into method loadUser

--- src/main/java/com/alkemy/ong/domain/usecase/impl/UserServiceImpl.java
+++ src/main/java/com/alkemy/ong/domain/usecase/impl/UserServiceImpl.java
@@ -1,17 +1,20 @@
 package com.alkemy.ong.domain.usecase.impl;
 
+import com.alkemy.ong.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+@RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserDetailsService {
 
 
     private final UserRepository userRepository;
-
+    
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -19,4 +22,4 @@ public class UserServiceImpl implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("User name: %s not found".formatted(email)));
     }
 }
-}
+
