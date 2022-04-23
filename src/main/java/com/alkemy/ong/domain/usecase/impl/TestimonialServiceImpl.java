13357e30c70172a5f8c0c0a029732482ffc19617
Aizuk <nickbrandariz@gmail.com>
2022-04-23 06:34:01 -0300

    testimonial service added

--- src/main/java/com/alkemy/ong/domain/usecase/impl/TestimonialServiceImpl.java
+++ src/main/java/com/alkemy/ong/domain/usecase/impl/TestimonialServiceImpl.java
@@ -2,9 +2,13 @@ package com.alkemy.ong.domain.usecase.impl;
 
 import com.alkemy.ong.domain.repository.TestimonialRepository;
 import com.alkemy.ong.domain.usecase.TestimonialService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+@Service
+@RequiredArgsConstructor
 public class TestimonialServiceImpl implements TestimonialService {
 
     private final TestimonialRepository testimonialRepository;
