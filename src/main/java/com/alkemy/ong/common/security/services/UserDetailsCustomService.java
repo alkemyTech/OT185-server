cc5e608a552b75bcac49b92b5b34b9b014d0de6e
Matias <maticritam@gmail.com>
2022-04-25 02:00:47 -0500

    feature/OT185-31

--- src/main/java/com/alkemy/ong/common/security/services/UserDetailsCustomService.java
+++ src/main/java/com/alkemy/ong/common/security/services/UserDetailsCustomService.java
@@ -40,33 +40,5 @@ public class UserDetailsCustomService implements UserDetailsService {
     }
 
 
-
-//    private Collection<? extends GrantedAuthority> getAuthorities(
-//            Collection<Role> roles) {
-//
-//        return getGrantedAuthorities(new ArrayList<>());
-//    }
-
-//    private List<String> getPrivileges(Collection<Role> roles) {
-//
-//        List<String> privileges = new ArrayList<>();
-//        List<Privilege> collection = new ArrayList<>();
-//        for (Role role : roles) {
-//            privileges.add(role.getName());
-//            collection.addAll(role.getPrivileges());
-//        }
-//        for (Privilege item : collection) {
-//            privileges.add(item.getName());
-//        }
-//        return privileges;
-//    }
-
-//    private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
-//        List<GrantedAuthority> authorities = new ArrayList<>();
-//        for (String privilege : privileges) {
-//            authorities.add(new SimpleGrantedAuthority(privilege));
-//        }
-//        return authorities;
-//    }
     }
 
