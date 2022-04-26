199cdfbd0151499f2a870e9f9e7df9e6a190b32b
Aizuk <nickbrandariz@gmail.com>
2022-04-26 18:17:22 -0300

    removed unnecesary annotation

--- src/main/java/com/alkemy/ong/domain/repository/TestimonialRepository.java
+++ src/main/java/com/alkemy/ong/domain/repository/TestimonialRepository.java
@@ -4,6 +4,5 @@ import com.alkemy.ong.domain.model.Testimonial;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TestimonialRepository extends PagingAndSortingRepository<Testimonial, Long> {
 }
