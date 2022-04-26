685d5ddd6e44246a619e3c77eb3f1a8549b418ed
joelR <rgzjoel14@gmail.com>
2022-04-26 20:29:14 -0300

    changes

--- src/main/java/com/alkemy/ong/configuration/SwaggerConfig.java
+++ src/main/java/com/alkemy/ong/configuration/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.alkemy.ong.ports.input.rs.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.alkemy.ong"))
                 .paths(PathSelectors.any())
                 .build();
     }
