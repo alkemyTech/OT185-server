ea9d62aecc91b7e18be94994e550207222de74c8
Sabrina-Incinga <sabrinagincinga@gmail.com>
2022-04-27 06:08:59 -0300

    [OT185-48] News delete endpoint added  (#39)
    Co-authored-by: Jonathan Manera <manerajona@gmail.com>

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/NewsController.java
@@ -0,0 +1,28 @@
+package com.alkemy.ong.ports.input.rs.controller;
+
+
+import com.alkemy.ong.domain.usecase.NewsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import static com.alkemy.ong.ports.input.rs.api.ApiConstants.NEWS_URI;
+
+
+@RestController
+@RequestMapping(NEWS_URI)
+@RequiredArgsConstructor
+public class NewsController {
+
+    private final NewsService service;
+
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteNews(@Valid @NotNull @PathVariable Long id) {
+        service.deleteById(id);
+    }
+}
