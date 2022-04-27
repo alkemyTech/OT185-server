4e8a20000da7cb91f2c4df46383ed947d4fab130
jisantillan <javierignaciosantillan.u@gmail.com>
2022-04-27 16:00:31 -0300

    Added logger and remove autowired annotation

--- src/main/java/com/alkemy/ong/ports/output/s3/AmazonS3Client.java
+++ src/main/java/com/alkemy/ong/ports/output/s3/AmazonS3Client.java
@@ -1,4 +1,71 @@
 package com.alkemy.ong.ports.output.s3;
 
+import com.alkemy.ong.configuration.S3Config;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
 public class AmazonS3Client {
+
+    private static final Logger log = LoggerFactory.getLogger(AmazonS3Client.class);
+
+    private final S3Config amazonConfig;
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${s3.bucket}")
+    private String bucketName;
+
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        try (FileOutputStream stream = new FileOutputStream(convFile)) {
+            FileOutputStream fos = new FileOutputStream(convFile);
+            fos.write(file.getBytes());
+            fos.close();
+            return convFile;
+        } catch (FileNotFoundException e) {
+            log.error("error converting file", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String generateFileName(MultipartFile multiPart) {
+        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
+    }
+
+    private void uploadFileTos3bucket(String fileName, File file) {
+        amazonS3.putObject(new PutObjectRequest(bucketName, fileName, file)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    public String uploadFile(MultipartFile multipartFile) {
+        String fileUrl = "";
+        try {
+            File file = convertMultiPartToFile(multipartFile);
+            String fileName = generateFileName(multipartFile);
+            uploadFileTos3bucket(fileName, file);
+            fileUrl = amazonS3.getUrl(bucketName, fileName).toString();
+            file.delete();
+        } catch (Exception e) {
+            log.error("error uploading file", e);
+            throw new RuntimeException(e);
+        }
+        return fileUrl;
+    }
 }
