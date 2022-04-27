f77b86aa68902d4f01a1319fa6d68ac3091a0ba5
jisantillan <javierignaciosantillan.u@gmail.com>
2022-04-27 15:57:38 -0300

    Added ConfigurationProperties and Setter annotations

--- src/main/java/com/alkemy/ong/configuration/S3Config.java
+++ src/main/java/com/alkemy/ong/configuration/S3Config.java
@@ -4,27 +4,26 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 
 @Configuration
+@Setter
+@ConfigurationProperties(prefix = "s3")
 public class S3Config {
 
-    @Value("${s3.accesskey}")
-    private String accessKey;
-
-    @Value("${s3.secretkey}")
-    private String secretKey;
-
-    @Value("${s3.region}")
+    private String accesskey;
+    private String secretkey;
     private String region;
 
     @Bean
     public AmazonS3 getS3Client(){
-        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accesskey, secretkey);
         return AmazonS3ClientBuilder.standard()
                                     .withRegion(region)
                                     .withCredentials(new AWSStaticCredentialsProvider(credentials))
