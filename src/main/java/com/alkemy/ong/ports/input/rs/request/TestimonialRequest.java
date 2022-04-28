d0de8529a28db01e062bc1ff6eceafa4555f084d
Aizuk <nickbrandariz@gmail.com>
2022-04-28 08:31:51 -0300

    Testimonial request added

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/request/TestimonialRequest.java
@@ -0,0 +1,39 @@
+package com.alkemy.ong.ports.input.rs.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestimonialRequest {
+
+    @NotBlank
+    @JsonProperty("name")
+    private String name;
+
+    @NotBlank
+    @JsonProperty("image")
+    private String image;
+
+    @NotBlank
+    @JsonProperty("content")
+    private String content;
+
+    @NotNull
+    @DateTimeFormat(
+            iso = ISO.DATE_TIME
+    )
+    @JsonProperty("start_date")
+    private Date startDate;
+}
