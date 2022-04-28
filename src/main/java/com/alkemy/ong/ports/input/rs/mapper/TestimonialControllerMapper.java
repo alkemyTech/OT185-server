ec65791ee2112af6c901d54f13d4dfcaa74e771a
Aizuk <nickbrandariz@gmail.com>
2022-04-28 17:10:48 -0300

    testimonial mapper added

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/TestimonialControllerMapper.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.ports.input.rs.mapper;
+
+import com.alkemy.ong.domain.model.Testimonial;
+import com.alkemy.ong.ports.input.rs.response.TestimonialResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Named;
+
+@Mapper
+public interface TestimonialControllerMapper extends CommonMapper{
+
+    @Named("testimonialToTestimonialResponse")
+    TestimonialResponse testimonialToTestimonialResponse (Testimonial testimonial);
+}
