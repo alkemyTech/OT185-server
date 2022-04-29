05f7cf0adcba925d0c23e72f717e3e48d12d1be2
Matias <maticritam@gmail.com>
2022-04-29 08:24:41 -0500

    Corrigiendo las observaciones del PR

--- src/main/java/com/alkemy/ong/common/security/utils/JwtUtil.java
+++ src/main/java/com/alkemy/ong/common/security/utils/JwtUtil.java
@@ -88,7 +88,6 @@ public class JwtUtil {
 
     public Boolean validateToken(String token, UserDetails userDetails){
 
-        System.out.println("Entro aca");
 
         final String username = extractUsername(token);
 
