e5218ebfbba46182aa553dbed326a201259f66fc
Aizuk <nickbrandariz@gmail.com>
2022-05-01 12:47:11 -0300

    unnecesary stuff removed

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/TestimonialControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/TestimonialControllerMapper.java
@@ -2,15 +2,11 @@ package com.alkemy.ong.ports.input.rs.mapper;
 
 import com.alkemy.ong.domain.model.Testimonial;
 import com.alkemy.ong.ports.input.rs.request.CreateTestimonialRequest;
-import com.alkemy.ong.ports.input.rs.response.TestimonialResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.Named;
+
 
 @Mapper
 public interface TestimonialControllerMapper extends CommonMapper{
-
-    @Named("testimonialToTestimonialResponse")
-    TestimonialResponse testimonialToTestimonialResponse (Testimonial testimonial);
 
     Testimonial createTestimonialRequestToTestimonial(CreateTestimonialRequest createTestimonialRequest);
 }
