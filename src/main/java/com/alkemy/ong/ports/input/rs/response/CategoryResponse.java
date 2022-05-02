975212269c51b4eb5de337b2cb6d1ef5351f1126
jisantillan <javierignaciosantillan.u@gmail.com>
2022-05-02 14:26:50 -0300

    Added category response

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/response/CategoryResponse.java
@@ -0,0 +1,26 @@
+package com.alkemy.ong.ports.input.rs.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryResponse {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("image")
+    private String image;
+
+
+}
