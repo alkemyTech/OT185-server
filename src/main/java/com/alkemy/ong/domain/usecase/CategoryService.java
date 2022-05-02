dbefd79c26e2202c066ffc54595208dc10599f93
Javier <65828792+jisantillan@users.noreply.github.com>
2022-05-02 05:48:40 -0300

    [OT185-42] Agregar una categoría nueva (#53)
    * Added CategoryRequest
    
    * Added CategoryControllerMapper
    
    * Added createCategory method
    
    * Added createCategory method
    
    * Implements createCategory method

--- src/main/java/com/alkemy/ong/domain/usecase/CategoryService.java
+++ src/main/java/com/alkemy/ong/domain/usecase/CategoryService.java
@@ -1,5 +1,9 @@
 package com.alkemy.ong.domain.usecase;
 
+import com.alkemy.ong.domain.model.Category;
+
 public interface CategoryService {
     void deleteById(Long id);
+
+    Long createCategory(Category request);
 }
