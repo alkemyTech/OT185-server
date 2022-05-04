2deb7ae5c0fc9891166e10bff5d123de0b5eeea4
Joel Rodriguez <62077942+joelrodriguez-rgb@users.noreply.github.com>
2022-05-04 17:25:09 -0300

    [OT185-69] Added endpoin createMember (#67)
    * add endpoin createMember
    
    * added @Transactional method createMember

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/MemberController.java
@@ -1,16 +1,19 @@
 package com.alkemy.ong.ports.input.rs.controller;
 
+import com.alkemy.ong.domain.model.Member;
+import com.alkemy.ong.ports.input.rs.mapper.MemberControllerMapper;
+import com.alkemy.ong.ports.input.rs.request.MemberRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.alkemy.ong.domain.usecase.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import static com.alkemy.ong.ports.input.rs.api.ApiConstants.MEMBERS_URI;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.net.URI;
 
 @RestController
 @RequestMapping(MEMBERS_URI)
@@ -18,10 +21,26 @@ import javax.validation.constraints.NotNull;
 public class MemberController {
 
     private final MemberService memberService;
+    private final MemberControllerMapper mapper;
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteMember(@Valid @NotNull @PathVariable Long id) {
         memberService.deleteById(id);
+    }
+
+
+
+    @PostMapping
+    public ResponseEntity<Void> createMember(@Valid @RequestBody MemberRequest request) {
+
+        Member member = mapper.memberRequestToMember(request);
+        final long id = memberService.createMember(member);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(id)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
