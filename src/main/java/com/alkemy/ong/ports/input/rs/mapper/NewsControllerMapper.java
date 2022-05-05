9ae5c01d365b8f6f6745904983b420159d6bc416
Matias <maticritam@gmail.com>
2022-05-05 08:57:15 -0500

    actualizando feature/OT185-45

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewsControllerMapper.java
@@ -0,0 +1,2 @@
+package com.alkemy.ong.ports.input.rs.mapper;public interface NewsControllerMapper {
+}
---
+++ src/main/java/com/alkemy/ong/ports/input/rs/response/NewsResponse.java
@@ -0,0 +1,2 @@
+package com.alkemy.ong.ports.input.rs.response;public class NewsResponse {
+}
