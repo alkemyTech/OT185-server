33253705c8e7b7558f6ee3787d1739c8dd754132
jisantillan <javierignaciosantillan.u@gmail.com>
2022-05-07 20:59:59 -0300

    Implements updateCategory method

--- src/main/java/com/alkemy/ong/domain/usecase/impl/CategoryServiceImpl.java
+++ src/main/java/com/alkemy/ong/domain/usecase/impl/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.domain.usecase.impl;
 
 import com.alkemy.ong.common.exception.NotFoundException;
+import com.alkemy.ong.domain.model.Activity;
 import com.alkemy.ong.domain.model.Category;
 import com.alkemy.ong.domain.repository.CategoryRepository;
 import com.alkemy.ong.domain.usecase.CategoryService;
@@ -30,6 +31,19 @@ public class CategoryServiceImpl implements CategoryService {
     @Transactional(readOnly = true)
     public Category getByIdIfExists(Long id) {
         return categoryJpaRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+    }
+
+    @Override
+    @Transactional
+    public Category updateCategory(Long id, Category request) {
+        if(categoryJpaRepository.existsById(id)) {
+            Category category = categoryJpaRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+            category.setName(request.getName());
+            category.setDescription(request.getDescription());
+            category.setImage(request.getImage());
+            return categoryJpaRepository.save(category);
+        }
+        return categoryJpaRepository.save(request);
     }
 
 }
