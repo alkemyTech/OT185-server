4eb4215e018caf24f924938efbffcad1d6a5d32d
Joel Rodriguez <62077942+joelrodriguez-rgb@users.noreply.github.com>
2022-05-08 08:22:16 -0300

    [OT185-68] Add endpoin get all Members (#76)
    * add endpoint getAll Member
    
    * change memberRequest
    
    * changes readOnly Ttrue

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/MemberControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/MemberControllerMapper.java
@@ -3,11 +3,16 @@ package com.alkemy.ong.ports.input.rs.mapper;
 import com.alkemy.ong.domain.model.Member;
 import com.alkemy.ong.ports.input.rs.request.MemberRequest;
 import com.alkemy.ong.ports.input.rs.request.UpdateMemberRequest;
+import com.alkemy.ong.ports.input.rs.response.MemberResponse;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MemberControllerMapper extends CommonMapper {
     Member memberRequestToMember(MemberRequest request);
 
     Member updateMemberRequestToMember(UpdateMemberRequest request);
+
+    List<MemberResponse> memberListToMemberResponseList(List<Member> member);
 }
