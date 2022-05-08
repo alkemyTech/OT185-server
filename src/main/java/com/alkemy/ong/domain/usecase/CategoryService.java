15dc6788839c6c7abfeaedd6b39b6ee27ca8c5f1
jisantillan <javierignaciosantillan.u@gmail.com>
2022-05-07 22:01:20 -0300

    Added method getAll

--- src/main/java/com/alkemy/ong/domain/usecase/CategoryService.java
+++ src/main/java/com/alkemy/ong/domain/usecase/CategoryService.java
@@ -2,10 +2,14 @@ package com.alkemy.ong.domain.usecase;
 
 import com.alkemy.ong.domain.model.Category;
 
+import java.util.List;
+
 public interface CategoryService {
     void deleteById(Long id);
 
     Long createCategory(Category request);
 
     Category getByIdIfExists(Long id);
+
+    List<Category> getAll();
 }
