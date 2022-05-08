dc3e89449206d41c1cad94ff8ac736c38420d15d
jisantillan <javierignaciosantillan.u@gmail.com>
2022-05-08 13:00:24 -0300

    Refactor update category

--- src/main/java/com/alkemy/ong/domain/usecase/impl/CategoryServiceImpl.java
+++ src/main/java/com/alkemy/ong/domain/usecase/impl/CategoryServiceImpl.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
@@ -35,14 +37,17 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     @Transactional
     public Category updateCategory(Long id, Category request) {
-        if(categoryJpaRepository.existsById(id)) {
-            Category category = categoryJpaRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+
+        Optional<Category> op = categoryJpaRepository.findById(id);
+        if(op.isPresent()) {
+            Category category = op.get();
             category.setName(request.getName());
             category.setDescription(request.getDescription());
             category.setImage(request.getImage());
             return categoryJpaRepository.save(category);
         }
         return categoryJpaRepository.save(request);
+
     }
 
 }
