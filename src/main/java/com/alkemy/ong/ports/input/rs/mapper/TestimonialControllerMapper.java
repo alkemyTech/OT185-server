8ddbb1e1d83ea76419d1d32fa78315e697f13129
Aizuk <nickbrandariz@gmail.com>
2022-05-08 09:58:20 -0300

    mapping methods

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/TestimonialControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/TestimonialControllerMapper.java
@@ -2,11 +2,17 @@ package com.alkemy.ong.ports.input.rs.mapper;
 
 import com.alkemy.ong.domain.model.Testimonial;
 import com.alkemy.ong.ports.input.rs.request.CreateTestimonialRequest;
+import com.alkemy.ong.ports.input.rs.request.UpdateTestimonialRequest;
+import com.alkemy.ong.ports.input.rs.response.TestimonialResponse;
 import org.mapstruct.Mapper;
+
+import java.util.Optional;
 
 
 @Mapper
 public interface TestimonialControllerMapper extends CommonMapper{
 
     Testimonial createTestimonialRequestToTestimonial(CreateTestimonialRequest createTestimonialRequest);
+    Testimonial updateTestimonialResquestToTestimonial(UpdateTestimonialRequest updateTestimonialRequest);
+    TestimonialResponse optionalTestimonialToTestimonialresponse(Optional<Testimonial> testimonial);
 }
