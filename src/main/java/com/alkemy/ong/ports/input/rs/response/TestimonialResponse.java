b260fcc85f0242aeefa012ba2974af80149659f2
Aizuk <nickbrandariz@gmail.com>
2022-05-08 09:55:07 -0300

    testimonial response

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/response/TestimonialResponse.java
@@ -0,0 +1,22 @@
+package com.alkemy.ong.ports.input.rs.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestimonialResponse {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("content")
+    private String content;
+
+}
