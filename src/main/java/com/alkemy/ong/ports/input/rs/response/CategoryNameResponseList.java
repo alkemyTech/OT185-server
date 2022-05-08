5bc16f061b438dcddf4e323885c0c8ecb2712b11
jisantillan <javierignaciosantillan.u@gmail.com>
2022-05-07 22:46:58 -0300

    Added CategoryNameResponse contains name of a category

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/response/CategoryNameResponseList.java
@@ -0,0 +1,17 @@
+package com.alkemy.ong.ports.input.rs.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryNameResponseList {
+
+        @JsonProperty("name")
+        private String name;
+
+}
