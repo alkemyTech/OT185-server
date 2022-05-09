d91654ca1eeb13424ab09dec63e4d9139fad2703
locknet11 <locknet41@protonmail.com>
2022-05-09 09:01:08 -0300

    fixed classes

--- src/main/resources/db/migration/V1.61__add_contacts.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO contact (contact_id, name, phone, email, message, created_at, is_active) VALUES
-(1, 'John Doe', '1134568790', 'johndoe@mail.com', 'Hola, me gustaria donar a la ONG', current_timestamp(), true),
-(2, 'Mary Jane', '2618945123', 'maryjane@mail.com', 'Hola, me gustaria saber mas info', current_timestamp(), true);
