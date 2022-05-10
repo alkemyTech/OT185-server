09dae4a0fb69cf0597acf512159fb97ab5b2d078
Matías Romero <58831680+ItamMati@users.noreply.github.com>
2022-05-10 16:50:55 -0500

    [OT185- 46] Add new in the system (#73)
    * feature/OT185-46
    
    * feature/OT185-46, faltan resolver dos dudas
    
    * actualizando feature/OT185-46
    
    * actualizando feature/OT185-46
    
    * actualizando correcciones PR
    
    * actualizando correcciones de PR
    
    * actualizando correcciones PR
    
    * actualizando a main

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewsControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewsControllerMapper.java
@@ -1,12 +1,14 @@
 package com.alkemy.ong.ports.input.rs.mapper;
 
 import com.alkemy.ong.domain.model.News;
+import com.alkemy.ong.ports.input.rs.request.CreateNewsRequest;
 import com.alkemy.ong.ports.input.rs.response.NewsResponse;
 import org.mapstruct.Mapper;
 
-@Mapper(uses = {CategoryControllerMapper.class})
+@Mapper
 public interface NewsControllerMapper {
 
-
+    News createNewsRequestToNews(CreateNewsRequest createNewsRequest);
     NewsResponse newsToNewsResponse(News newsResponse);
 }
+
---
+++ src/main/java/com/alkemy/ong/ports/input/rs/request/CreateNewsRequest.java
@@ -0,0 +1,32 @@
+package com.alkemy.ong.ports.input.rs.request;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateNewsRequest {
+
+    @NotBlank
+    @JsonProperty("name")
+    private String name;
+
+    @NotBlank
+    @JsonProperty("content")
+    private String content;
+
+    @NotBlank
+    @JsonProperty("image")
+    private String image;
+
+
+    @JsonProperty("category_id")
+    private Long categoryId;
+}
