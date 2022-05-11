255b018a6dd8cfa340fa3657fc8f03060b1fd899
locknet11 <locknet41@protonmail.com>
2022-05-11 09:00:12 -0300

    added method call in service

--- src/main/java/com/alkemy/ong/domain/usecase/impl/ContactServiceImpl.java
+++ src/main/java/com/alkemy/ong/domain/usecase/impl/ContactServiceImpl.java
@@ -1,11 +1,18 @@
 package com.alkemy.ong.domain.usecase.impl;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.alkemy.ong.domain.model.Contact;
+import com.alkemy.ong.domain.model.Organization;
 import com.alkemy.ong.domain.repository.ContactRepository;
+import com.alkemy.ong.domain.repository.OrganizationRepository;
 import com.alkemy.ong.domain.usecase.ContactService;
+import com.alkemy.ong.ports.output.email.SendGridEmailService;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -13,6 +20,12 @@ import lombok.RequiredArgsConstructor;
 public class ContactServiceImpl implements ContactService {
 
 	private final ContactRepository contactJpaRepository;
+	
+	private final OrganizationRepository organizationJpaRepository;
+	
+	private final SendGridEmailService emailService;
+	
+	private static final Long ORGANIZATION_ID = (long) 1;
 
 	@Override
 	@Transactional(readOnly = true)
@@ -22,7 +35,11 @@ public class ContactServiceImpl implements ContactService {
 
 	@Override
 	@Transactional
-	public Long createEntity(Contact contact){
+	public Long createEntity(Contact contact) {
+		Optional<Organization> organization = organizationJpaRepository.findById(ORGANIZATION_ID);
+		if(organization.isPresent()) {
+			emailService.sendContactedSuccessfully(contact.getName(), contact.getEmail(), organization.get());
+		}
 		return contactJpaRepository.save(contact).getId();
 	}
 
