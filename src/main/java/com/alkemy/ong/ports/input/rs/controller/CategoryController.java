49f09bf61b021cacbd283029fcf15dc38395261b
jisantillan <javierignaciosantillan.u@gmail.com>
2022-05-12 15:07:24 -0300

    Refactor getCategories method, now use pagging

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/CategoryController.java
@@ -1,12 +1,16 @@
 package com.alkemy.ong.ports.input.rs.controller;
 
 import com.alkemy.ong.domain.model.Category;
+import com.alkemy.ong.domain.model.CategoryList;
 import com.alkemy.ong.domain.usecase.CategoryService;
+import com.alkemy.ong.ports.input.rs.api.ApiConstants;
 import com.alkemy.ong.ports.input.rs.mapper.CategoryControllerMapper;
 import com.alkemy.ong.ports.input.rs.request.CategoryRequest;
+import com.alkemy.ong.ports.input.rs.response.CategoryNameResponse;
 import com.alkemy.ong.ports.input.rs.response.CategoryNameResponseList;
 import com.alkemy.ong.ports.input.rs.response.CategoryResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,7 @@ import javax.validation.constraints.NotNull;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import static com.alkemy.ong.ports.input.rs.api.ApiConstants.CATEGORIES_URI;
 
@@ -35,12 +40,33 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<List<CategoryNameResponseList>> getCategories(){
-        List<Category> categories = categoryService.getAll();
-        List<CategoryNameResponseList> responses = mapper.categoryListToCategoryResponse(categories);
+    public ResponseEntity<CategoryNameResponseList> getCategories(@RequestParam Optional<Integer> page,
+                                                                  @RequestParam Optional<Integer> size){
 
-        return ResponseEntity.ok().body(responses);
+        final int pageNumber = page.filter(p -> p > 0).orElse(ApiConstants.DEFAULT_PAGE);
+        final int pageSize = size.filter(s -> s > 0).orElse(ApiConstants.DEFAULT_PAGE_SIZE);
+
+        CategoryList categories = categoryService.getList(PageRequest.of(pageNumber, pageSize));
+        CategoryNameResponseList response;
+        {
+            response = new CategoryNameResponseList();
+            List<CategoryNameResponse> content = mapper.categoryListToCategoryNameResponse(categories.getContent());
+            response.setContent(content);
+
+            final int nextPage = categories.getPageable().next().getPageNumber();
+            response.setNextUri(ApiConstants.uriByPageAsString.apply(nextPage));
+
+            final int previousPage = categories.getPageable().previousOrFirst().getPageNumber();
+            response.setPreviousUri(ApiConstants.uriByPageAsString.apply(previousPage));
+
+            response.setTotalPages(categories.getTotalPages());
+            response.setTotalElements(categories.getTotalElements());
+
+        }
+        return ResponseEntity.ok().body(response);
+
     }
+
 
     @PostMapping
     public ResponseEntity<Void> createCategory(@Valid @RequestBody CategoryRequest categoryRequest){
