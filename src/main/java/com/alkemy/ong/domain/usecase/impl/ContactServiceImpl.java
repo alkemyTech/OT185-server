d1ff5ebe8eeda1d178a93fa1f7e41a1ee366a8a2
Santiago Pulido <52685437+locknet11@users.noreply.github.com>
2022-05-12 19:17:40 -0300

    [OT185-62] Send email when new Contact is created (#86)
    * Added method, correct methods
    
    * added method call in service
    
    * fix classes
    
    * fix templates names
    
    * fixes

--- src/main/java/com/alkemy/ong/domain/usecase/impl/ContactServiceImpl.java
+++ src/main/java/com/alkemy/ong/domain/usecase/impl/ContactServiceImpl.java
@@ -1,11 +1,16 @@
 package com.alkemy.ong.domain.usecase.impl;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.alkemy.ong.domain.model.Contact;
+import com.alkemy.ong.domain.model.Organization;
 import com.alkemy.ong.domain.repository.ContactRepository;
+import com.alkemy.ong.domain.repository.OrganizationRepository;
 import com.alkemy.ong.domain.usecase.ContactService;
+import com.alkemy.ong.ports.output.email.SendGridEmailService;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -13,6 +18,12 @@ import lombok.RequiredArgsConstructor;
 public class ContactServiceImpl implements ContactService {
 
 	private final ContactRepository contactJpaRepository;
+	
+	private final OrganizationRepository organizationJpaRepository;
+	
+	private final SendGridEmailService emailService;
+	
+	private static final Long ORGANIZATION_ID = (long) 1;
 
 	@Override
 	@Transactional(readOnly = true)
@@ -22,7 +33,9 @@ public class ContactServiceImpl implements ContactService {
 
 	@Override
 	@Transactional
-	public Long createEntity(Contact contact){
+	public Long createEntity(Contact contact) {
+		Optional<Organization> op = organizationJpaRepository.findById(ORGANIZATION_ID);
+		op.ifPresent(organization -> emailService.sendContactedSuccessfully(contact, organization));
 		return contactJpaRepository.save(contact).getId();
 	}
 
