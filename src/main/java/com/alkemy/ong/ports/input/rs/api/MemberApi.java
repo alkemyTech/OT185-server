f92dc569d364b11a88af355b4a8dd7650a912219
Joel Rodriguez <rgzjoel14@gmail.com>
2022-05-13 11:38:34 -0300

    v1 add doc memberController

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/MemberApi.java
@@ -0,0 +1,97 @@
+package com.alkemy.ong.ports.input.rs.api;
+
+import com.alkemy.ong.common.exception.error.ErrorDetails;
+import com.alkemy.ong.ports.input.rs.request.MemberRequest;
+import com.alkemy.ong.ports.input.rs.request.UpdateMemberRequest;
+import com.alkemy.ong.ports.input.rs.response.MemberResponse;
+import com.alkemy.ong.ports.input.rs.response.MemberResponseList;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+@Validated
+@SecurityRequirement(name = "bearerAuth")
+public interface MemberApi {
+
+    @Operation(summary = "create a new Member", responses = {
+            @ApiResponse(responseCode = "201", description = "Member created",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MemberResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "401", description = "Invalid token or token expired",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "403", description = "Invalid Role",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content)
+    })
+    ResponseEntity<Void> createMember(@Valid @RequestBody MemberRequest request);
+
+
+    @Operation(summary = "update  Member", description = "update a Member completely", responses = {
+            @ApiResponse(responseCode = "200", description = "update Member ",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MemberResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "401", description = "Invalid token or token expired",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "403", description = "Invalid Role",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "404", description = "Not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content)
+    })
+    void updateMember(@Valid @NotNull @PathVariable Long id,
+                      @Valid @RequestBody UpdateMemberRequest upMember);
+
+
+    @Operation(summary = "delete  Member", description = "delete a Member ", responses = {
+            @ApiResponse(responseCode = "204", description = "delete Member ", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid request",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "401", description = "Invalid token or token expired",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "403", description = "Invalid Role",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "404", description = "Not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content)
+    })
+    void deleteMember(@Valid @NotNull @PathVariable Long id);
+
+
+    @Operation(summary = "get all  Members", description = "get  Members ", responses = {
+            @ApiResponse(responseCode = "200", description = "get Members ",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MemberResponseList.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "401", description = "Invalid token or token expired",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content)
+    })
+    ResponseEntity<MemberResponseList> getMembers(@RequestParam Optional<Integer> page,
+                                                  @RequestParam Optional<Integer> size);
+
+}
