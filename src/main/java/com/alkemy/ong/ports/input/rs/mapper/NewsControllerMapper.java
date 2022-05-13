ecc8790886b34ff259bc7d7c482515e4c2e59fa0
Joel Rodriguez <rgzjoel14@gmail.com>
2022-05-13 12:27:53 -0300

    fix NewsControllerMapper add @Named

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewsControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewsControllerMapper.java
@@ -7,6 +7,7 @@ import com.alkemy.ong.ports.input.rs.request.UpdateNewsRequest;
 import com.alkemy.ong.ports.input.rs.response.NewsResponse;
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 
 import java.util.List;
 
@@ -14,7 +15,7 @@ import java.util.List;
 @Mapper
 public interface NewsControllerMapper {
 
-    @IterableMapping(qualifiedByName = "newsToNewsResponse")
+    @Named("newsToNewsResponse")
     List<NewsResponse> newsListToNewsResponseList(List<News> news);
 
     News createNewsRequestToNews(CreateNewsRequest createNewsRequest);
