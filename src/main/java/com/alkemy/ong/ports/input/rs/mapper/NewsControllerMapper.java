8c492f8c30e81ed0aacc4c1ef13c5da8afec8e87
Jonathan Manera <manerajona@gmail.com>
2022-05-14 09:48:59 -0300

    Reverted change on NewsControllerMapper

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewsControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/NewsControllerMapper.java
@@ -4,7 +4,7 @@ import com.alkemy.ong.domain.model.News;
 import com.alkemy.ong.ports.input.rs.request.CreateNewsRequest;
 import com.alkemy.ong.ports.input.rs.request.UpdateNewsRequest;
 import com.alkemy.ong.ports.input.rs.response.NewsResponse;
-
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Named;
 
@@ -13,7 +13,7 @@ import java.util.List;
 @Mapper
 public interface NewsControllerMapper {
 
-    @Named("newsToNewsResponse")
+    @IterableMapping(qualifiedByName = "newsToNewsResponse")
     List<NewsResponse> newsListToNewsResponseList(List<News> news);
 
     News createNewsRequestToNews(CreateNewsRequest createNewsRequest);
