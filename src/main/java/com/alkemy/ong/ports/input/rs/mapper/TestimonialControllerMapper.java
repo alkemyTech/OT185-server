31acbbf39dfc70eb1c0bbc07a0c893f6036b7695
Ignacio Nicolas Brandariz <nickbrandariz@gmail.com>
2022-05-16 09:44:03 -0300

    [OT185-107] Adding paging to testimonials (#99)
    * testimonial response list
    
    * map method testimoniallist
    
    * testimonial list entity
    
    * testimonial service paging
    
    * controller GET testimonials method
    
    * minor type fix
    
    * bug fix, now page size is fixed at 10 as requested
    
    * readonly to getList

--- src/main/java/com/alkemy/ong/ports/input/rs/mapper/TestimonialControllerMapper.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/mapper/TestimonialControllerMapper.java
@@ -4,7 +4,11 @@ import com.alkemy.ong.domain.model.Testimonial;
 import com.alkemy.ong.ports.input.rs.request.CreateTestimonialRequest;
 import com.alkemy.ong.ports.input.rs.request.UpdateTestimonialRequest;
 import com.alkemy.ong.ports.input.rs.response.TestimonialResponse;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 
 @Mapper
@@ -12,5 +16,10 @@ public interface TestimonialControllerMapper extends CommonMapper{
 
     Testimonial createTestimonialRequestToTestimonial(CreateTestimonialRequest createTestimonialRequest);
     Testimonial updateTestimonialResquestToTestimonial(UpdateTestimonialRequest updateTestimonialRequest);
+
+    @Named("testimonialToTestimonialResponse")
     TestimonialResponse testimonialToTestimonialResponse(Testimonial testimonial);
+
+    @IterableMapping(qualifiedByName = "testimonialToTestimonialResponse")
+    List<TestimonialResponse> testimonialListToTestimonialResponseList(List<Testimonial> testimonials);
 }
