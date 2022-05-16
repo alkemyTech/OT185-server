e9bf639441384a8a9a27d4aa75dc4d43df3fbc7c
Santiago Pulido <52685437+locknet11@users.noreply.github.com>
2022-05-16 07:54:07 -0300

    [OT185-108] Add pagination to Users (#101)

--- src/main/java/com/alkemy/ong/ports/input/rs/controller/UserController.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/controller/UserController.java
@@ -1,25 +1,22 @@
 package com.alkemy.ong.ports.input.rs.controller;
 
-
 import com.alkemy.ong.domain.model.User;
+import com.alkemy.ong.domain.model.UserList;
 import com.alkemy.ong.domain.usecase.UserService;
+import com.alkemy.ong.ports.input.rs.api.ApiConstants;
 import com.alkemy.ong.ports.input.rs.mapper.UserControllerMapper;
 import com.alkemy.ong.ports.input.rs.request.UpdateUserRequest;
-
-
-import com.alkemy.ong.domain.usecase.UserService;
-
 import com.alkemy.ong.ports.input.rs.response.UserResponse;
+import com.alkemy.ong.ports.input.rs.response.UserResponseList;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-
 import java.util.List;
-
+import java.util.Optional;
 import static com.alkemy.ong.ports.input.rs.api.ApiConstants.USER_URI;
 
 @RestController
@@ -27,33 +24,51 @@ import static com.alkemy.ong.ports.input.rs.api.ApiConstants.USER_URI;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+	private final UserService userService;
 
+	private final UserControllerMapper userControllerMapper;
 
-    private final UserControllerMapper userControllerMapper;
+	@PatchMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void updateUser(@Valid @NotNull @PathVariable Long id,
+			@Valid @RequestBody UpdateUserRequest updateUserRequest) {
 
-    @PatchMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateUser(@Valid @NotNull @PathVariable Long id, @Valid @RequestBody UpdateUserRequest updateUserRequest) {
+		User user = userControllerMapper.updateUserRequestToUser(updateUserRequest);
 
-        User user = userControllerMapper.updateUserRequestToUser(updateUserRequest);
+		userService.updateEntityIfExists(id, user);
+	}
 
-        userService.updateEntityIfExists(id, user);
-    }
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteUserById(@Valid @NotNull @PathVariable Long id) {
+		userService.deleteUserById(id);
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUserById(@Valid @NotNull @PathVariable Long id) {
-        userService.deleteUserById(id);
+	}
 
-    }
+	@GetMapping
+	public ResponseEntity<UserResponseList> getUsers(@RequestParam Optional<Integer> page,
+			@RequestParam Optional<Integer> size) {
 
-    @GetMapping
-    public ResponseEntity<List<UserResponse>> getUsers(){
-        List<User> users = userService.getAll();
-        List<UserResponse> responses = userControllerMapper.userListToUserResponse(users);
+		final int pageNumber = page.filter(p -> p > 0).orElse(ApiConstants.DEFAULT_PAGE);
+		final int pageSize = size.filter(s -> s > 0).orElse(ApiConstants.DEFAULT_PAGE_SIZE);
 
-        return ResponseEntity.ok().body(responses);
-    }
+		UserList list = userService.getList(PageRequest.of(pageNumber, pageSize));
+		UserResponseList response;
+		{
+			response = new UserResponseList();
+			List<UserResponse> content = userControllerMapper.userListToUserResponseList(list.getContent());
+			response.setContent(content);
+			final int nextPage = list.getPageable().next().getPageNumber();
+			response.setNextUri(ApiConstants.uriByPageAsString.apply(nextPage));
+
+			final int previousPage = list.getPageable().previousOrFirst().getPageNumber();
+			response.setPreviousUri(ApiConstants.uriByPageAsString.apply(previousPage));
+
+			response.setTotalPages(list.getTotalPages());
+			response.setTotalElements(list.getTotalElements());
+		}
+
+		return ResponseEntity.ok().body(response);
+	}
 
 }
