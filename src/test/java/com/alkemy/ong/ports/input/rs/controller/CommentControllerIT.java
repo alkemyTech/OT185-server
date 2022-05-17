e54e19950b6ab70e1fcbbf7946d9ac8d7a082664
Sabrina-Incinga <sabrinagincinga@gmail.com>
2022-05-17 16:32:56 -0300

    integration test added, final commit

--- src/test/java/com/alkemy/ong/ports/input/rs/controller/CommentControllerIT.java
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/CommentControllerIT.java
@@ -3,10 +3,13 @@ package com.alkemy.ong.ports.input.rs.controller;
 import com.alkemy.ong.H2Config;
 import com.alkemy.ong.common.util.JsonUtils;
 import com.alkemy.ong.domain.model.Comment;
+import com.alkemy.ong.domain.model.CommentList;
 import com.alkemy.ong.domain.model.News;
 import com.alkemy.ong.domain.model.User;
 import com.alkemy.ong.ports.input.rs.api.ApiConstants;
 import com.alkemy.ong.ports.input.rs.request.CreateCommentRequest;
+import com.alkemy.ong.ports.input.rs.request.UpdateCommentRequest;
+import com.alkemy.ong.ports.input.rs.response.CommentResponseList;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -14,17 +17,24 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -61,21 +71,96 @@ class CommentControllerIT {
     }
 
     @Test
-    @Order(4)
+    @Order(2)
     @WithUserDetails("admin@somosmas.org")
-    void updateComment() {
+    void updateComment_shouldReturn204() throws Exception {
+        UpdateCommentRequest request = UpdateCommentRequest.builder()
+                .commentBody("update")
+                .build();
+
+        mockMvc.perform(put(ApiConstants.COMMENT_URI + "/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.objectToJson(request)))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+
     }
 
     @Test
-    @Order(2)
+    @Order(3)
     @WithUserDetails("fbar@somosmas.org")
-    void getComments() {
+    void updateComment_shouldReturn403() throws Exception {
+        UpdateCommentRequest request = UpdateCommentRequest.builder()
+                .commentBody("second update")
+                .build();
+
+        mockMvc.perform(put(ApiConstants.COMMENT_URI + "/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.objectToJson(request)))
+                .andExpect(status().isForbidden())
+                .andDo(print());
+
+    }
+
+    @Test
+    @Order(4)
+    @WithAnonymousUser
+    void getComments_shouldReturn401() throws Exception {
+        mockMvc.perform(get(ApiConstants.COMMENT_URI))
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
     }
 
     @Test
     @Order(5)
+    @WithUserDetails("fbar@somosmas.org")
+    void getComments_shouldReturn200() throws Exception{
+
+        String content = mockMvc.perform(get(ApiConstants.COMMENT_URI))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertThat(content).isNotBlank();
+
+        CommentResponseList response = JsonUtils.jsonToObject(content, CommentResponseList.class);
+
+        assertThat(response.getTotalElements()).isEqualTo(1);
+        assertThat(response.getTotalPages()).isEqualTo(1);
+        assertThat(response.getNextUri()).isEqualTo("http://localhost/v1/comments?page=1");
+        assertThat(response.getPreviousUri()).isEqualTo("http://localhost/v1/comments?page=0");
+        assertThat(response.getContent()).isNotEmpty();
+    }
+
+
+    @Test
+    @Order(6)
+    @WithUserDetails("fbar@somosmas.org")
+    void deleteComment_shouldReturn403() throws Exception {
+
+        mockMvc.perform(delete(ApiConstants.COMMENT_URI + "/1"))
+                .andExpect(status().isForbidden())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+    }
+
+    @Test
+    @Order(7)
     @WithUserDetails("admin@somosmas.org")
-    void deleteComment() {
+    void deleteComment_shouldReturn204() throws Exception {
+
+        mockMvc.perform(delete(ApiConstants.COMMENT_URI + "/1"))
+                .andExpect(status().isNoContent())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
     }
 
 }
