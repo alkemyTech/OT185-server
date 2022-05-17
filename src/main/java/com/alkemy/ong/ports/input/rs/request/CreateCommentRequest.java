85cc56fc599b28bc49a228cf059f208613f2f417
Sabrina-Incinga <sabrinagincinga@gmail.com>
2022-05-17 11:47:00 -0300

    unit tests added. Modifications required on requests were made

--- src/main/java/com/alkemy/ong/ports/input/rs/request/CreateCommentRequest.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/request/CreateCommentRequest.java
@@ -4,12 +4,14 @@ package com.alkemy.ong.ports.input.rs.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateCommentRequest {
