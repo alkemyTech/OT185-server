e5ac5ed1311a9a63aba26e72134ef75d84bf2b79
Sabrina-Incinga <sabrinagincinga@gmail.com>
2022-05-17 13:05:25 -0300

    draft

--- src/test/java/com/alkemy/ong/ports/input/rs/controller/CommentControllerIT.java
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/CommentControllerIT.java
@@ -1,6 +1,12 @@
 package com.alkemy.ong.ports.input.rs.controller;
 
 import com.alkemy.ong.H2Config;
+import com.alkemy.ong.common.util.JsonUtils;
+import com.alkemy.ong.domain.model.Comment;
+import com.alkemy.ong.domain.model.News;
+import com.alkemy.ong.domain.model.User;
+import com.alkemy.ong.ports.input.rs.api.ApiConstants;
+import com.alkemy.ong.ports.input.rs.request.CreateCommentRequest;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -8,10 +14,19 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
@@ -26,7 +41,26 @@ class CommentControllerIT {
     @Test
     @Order(1)
     @WithUserDetails("admin@somosmas.org")
-    void createComment() {
+    void createComment_shouldReturn201() throws Exception {
+
+        News news = new News();
+        news.setName("Some news");
+
+        CreateCommentRequest request = CreateCommentRequest.builder()
+                .commentBody("example comment")
+                .newsId(1L)
+                .build();
+
+        final String actualLocation = mockMvc.perform(post(ApiConstants.COMMENT_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.objectToJson(request)))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getHeader(HttpHeaders.LOCATION);
+
+        assertThat(actualLocation).isEqualTo("http://localhost/v1/comments/1");
     }
 
     @Test
