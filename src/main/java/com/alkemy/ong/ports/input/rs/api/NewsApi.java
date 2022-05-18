54aaa5bbd5ba1402e1ec87de82edb8a206bfada9
Matias <maticritam@gmail.com>
2022-05-18 01:41:26 -0500

    corrigiendo lo marcado en el PR

--- src/main/java/com/alkemy/ong/ports/input/rs/api/NewsApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/NewsApi.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.ports.input.rs.api;
 
 
+import com.alkemy.ong.common.exception.error.ErrorDetails;
 import com.alkemy.ong.ports.input.rs.request.CreateNewsRequest;
 import com.alkemy.ong.ports.input.rs.request.UpdateNewsRequest;
 import com.alkemy.ong.ports.input.rs.response.CommentResponse;
@@ -8,9 +9,11 @@ import com.alkemy.ong.ports.input.rs.response.NewsResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -26,44 +29,93 @@ public interface NewsApi {
 
     @Operation(summary = "Update news by ID", description = "Update all details of the specific news ")
     @ApiResponses(value = {
+
             @ApiResponse(responseCode = "200", description = "News updated",
-                    content = { @Content(mediaType = "JSON Value")}),
+                    content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+
+
+
             @ApiResponse(responseCode = "400", description = "Bad Request",
-                    content = @Content(mediaType = "JSON Value",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
                             examples = @ExampleObject(value="Incorrect parameters in JSON Object"))),
+
+
+
             @ApiResponse(responseCode = "404",description = "Not Found",
-                    content = @Content(mediaType = "JSON Value",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
                             examples = @ExampleObject(value = ("News's ID is not found."))
-                    ))
-    }
-    )
+                    )),
+
+            @ApiResponse(responseCode = "401", description = "Invalid token or token expired",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))
+            }),
+            @ApiResponse(responseCode = "403", description = "Invalid Role",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))
+                    })
+
+    })
     ResponseEntity<NewsResponse> updateNews(@Valid @NotNull @PathVariable Long id, @Valid @RequestBody UpdateNewsRequest updateNewsRequest);
 
     @Operation(summary = "Get news by ID", description = "Get details of the specific news ")
     @ApiResponses(value = {
+
             @ApiResponse(responseCode = "200", description = "News found",
-                    content = { @Content(mediaType = "JSON Value")}),
+                    content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+
+
             @ApiResponse(responseCode = "400", description = "Bad Request",
-                    content = @Content(mediaType = "JSON Value",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
                             examples = @ExampleObject(value="Incorrect parameter (ID)"))),
+
+
             @ApiResponse(responseCode = "404",description = "Not Found",
-                    content = @Content(mediaType = "JSON Value",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
                             examples = @ExampleObject(value = ("Id is not found."))
-                    ))
+                    )),
+
+            @ApiResponse(responseCode = "401", description = "Invalid token or token expired",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+
+
     })
     ResponseEntity<NewsResponse> getNew(@Valid @NotNull @PathVariable Long id);
 
     @Operation(summary = "Delete news", description = "Delete news by ID ")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "No content",
-                    content = { @Content(mediaType = "JSON Value")}),
+            @ApiResponse(responseCode = "204", description = "No content"),
+
+
             @ApiResponse(responseCode = "400", description = "Bad Request",
-                    content = @Content(mediaType = "JSON Value",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
                             examples = @ExampleObject(value="Incorrect parameter (ID)"))),
+
+
+
             @ApiResponse(responseCode = "404",description = "Not Found",
-                    content = @Content(mediaType = "JSON Value",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
                             examples = @ExampleObject(value = ("Id is not found."))
-                    ))
+                    )),
+
+
+
+            @ApiResponse(responseCode = "401", description = "Invalid token or token expired",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+
+
+            @ApiResponse(responseCode = "403", description = "Invalid Role",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
     }
     )
     void deleteNews(@Valid @NotNull @PathVariable Long id);
@@ -71,10 +123,24 @@ public interface NewsApi {
     @Operation(summary = "Get news's comments", description = "Get news's comments by ID ")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Comments found",
-                    content = { @Content(mediaType = "JSON Value")}),
+                    content = { @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+
+
             @ApiResponse(responseCode = "400", description = "Bad Request",
-                    content = @Content(mediaType = "JSON Value",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
                             examples = @ExampleObject(value="Incorrect parameter (ID)"))),
+
+
+            @ApiResponse(responseCode = "404",description = "Not Found",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value = ("Id is not found."))
+                    )),
+
+            @ApiResponse(responseCode = "401", description = "Invalid token or token expired",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
     }
     )
     ResponseEntity<List<CommentResponse>> getCommentsByNewsId(@Valid @NotNull @PathVariable Long id);
@@ -82,11 +148,19 @@ public interface NewsApi {
 
     @Operation(summary = "Create news", description = "Create news ")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "News created",
-                    content = { @Content(mediaType = "JSON Value")}),
+            @ApiResponse(responseCode = "201", description = "News created"),
+
+
             @ApiResponse(responseCode = "400", description = "Bad Request",
-                    content = @Content(mediaType = "JSON Value",
-                            examples = @ExampleObject(value="Incorrect parameters in JSON Object")))}
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class),
+                            examples = @ExampleObject(value="Incorrect parameters in JSON Object"))),
+
+
+            @ApiResponse(responseCode = "401", description = "Invalid token or token expired",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+    }
     )
    ResponseEntity<Void> createNews(@Valid @RequestBody CreateNewsRequest createNewsRequest);
 
