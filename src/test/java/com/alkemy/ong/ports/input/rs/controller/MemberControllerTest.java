c8edda356e398953ab37a27f9461eec990586d9a
Joel Rodriguez <rgzjoel14@gmail.com>
2022-05-18 13:58:34 -0300

    Test UnitTest V1 test MemberService

--- src/test/java/com/alkemy/ong/ports/input/rs/controller/MemberControllerTest.java
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/MemberControllerTest.java
@@ -2,12 +2,14 @@ package com.alkemy.ong.ports.input.rs.controller;
 
 import com.alkemy.ong.common.exception.handler.GlobalExceptionHandler;
 import com.alkemy.ong.common.util.JsonUtils;
-import com.alkemy.ong.domain.model.Member;
+import com.alkemy.ong.domain.model.*;
 import com.alkemy.ong.domain.usecase.MemberService;
 import com.alkemy.ong.ports.input.rs.api.ApiConstants;
 import com.alkemy.ong.ports.input.rs.mapper.MemberControllerMapper;
 import com.alkemy.ong.ports.input.rs.request.MemberRequest;
 import com.alkemy.ong.ports.input.rs.request.UpdateMemberRequest;
+import com.alkemy.ong.ports.input.rs.response.AlkymerResponseList;
+import com.alkemy.ong.ports.input.rs.response.MemberResponseList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,18 +18,21 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -102,6 +107,50 @@ class MemberControllerTest {
                         .content(JsonUtils.objectToJson(request)))
                 .andExpect(status().isNoContent())
                 .andDo(print());
+    }
+
+    @Test
+    void deleteMember_shouldReturn204() throws Exception {
+
+        mockMvc.perform(delete(ApiConstants.MEMBERS_URI + "/1"))
+                .andExpect(status().isNoContent())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+    }
+
+    @Test
+    void getAlkymers_shouldReturn200() throws Exception {
+
+
+        Member member = new Member();
+        member.setId(99L);
+        member.setName("member");
+        member.setImage("image");
+        member.setDescription("description");
+
+
+        MemberList list = new MemberList(List.of(member), Pageable.ofSize(1), 1);
+
+        given(service.getAll(any(PageRequest.class))).willReturn(list);
+
+        String content = mockMvc.perform(get(ApiConstants.MEMBERS_URI+"?page=0&size=1"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        assertThat(content).isNotBlank();
+
+        MemberResponseList response = JsonUtils.jsonToObject(content, MemberResponseList.class);
+
+        assertThat(response.getTotalElements()).isEqualTo(1);
+        assertThat(response.getTotalPages()).isEqualTo(1);
+        assertThat(response.getNextUri()).isEqualTo("http://localhost/v1/members?size=1&page=1");
+        assertThat(response.getPreviousUri()).isEqualTo("http://localhost/v1/members?size=1&page=0");
+        assertThat(response.getContent()).isNotEmpty();
     }
 
 }
