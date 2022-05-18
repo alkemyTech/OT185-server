8fd9c3c72085e33fe95e2b313e512b3287e349e5
David Sanchez <tapi.cr7@gmail.com>
2022-05-18 00:09:59 -0300

    refactor

--- src/main/java/com/alkemy/ong/ports/input/rs/api/OrganizationApi.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/api/OrganizationApi.java
@@ -6,11 +6,12 @@ import com.alkemy.ong.ports.input.rs.request.UpdateOrganizationRequest;
 import com.alkemy.ong.ports.input.rs.response.OrganizationResponse;
 import com.alkemy.ong.ports.input.rs.response.SlideResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,50 +25,72 @@ import java.util.List;
 public interface OrganizationApi {
 
     @Operation(summary = "Get Organization by id", responses = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved Organization", content = {@Content(mediaType = "JSON Value",
-                    schema = @Schema(implementation = OrganizationResponse.class))}),
-            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "JSON Value",
-                    examples = @ExampleObject(value = "Incorrect parameters in JSON Object"))),
-            @ApiResponse(responseCode = "404", description = "Not found",
-                    content = {@Content(mediaType = "JSON Value",
-                            examples = @ExampleObject(value = ("Organization ID is not found.")),
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved Organization",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = OrganizationResponse.class))}),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
                             schema = @Schema(implementation = ErrorDetails.class))}),
-            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content)
+            @ApiResponse(responseCode = "404", description = "Not found",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = ErrorDetails.class))}),
     })
     ResponseEntity<OrganizationResponse> getOrganization(@Valid @NotNull @PathVariable Long id);
 
     @Operation(summary = "Get List of slides by Organization id", responses = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved Organization slide list", content = {@Content(mediaType = "JSON Value",
-                    schema = @Schema(implementation = SlideResponse.class))}),
-            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "JSON Value",
-                    examples = @ExampleObject(value = "Incorrect parameters in JSON Object"))),
-            @ApiResponse(responseCode = "404", description = "Not found",
-                    content = {@Content(mediaType = "JSON Value",
-                            examples = @ExampleObject(value = ("Organization ID is not found.")),
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved Organization slide list",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = SlideResponse.class)))}),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
                             schema = @Schema(implementation = ErrorDetails.class))}),
-            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content)
+            @ApiResponse(responseCode = "404", description = "Not found",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = ErrorDetails.class))}),
     })
     ResponseEntity<List<SlideResponse>> getSlideList(@Valid @NotNull @PathVariable Long id);
+
     @SecurityRequirement(name = "bearerAuth")
     @Operation(summary = "update organization partially", responses = {
-            @ApiResponse(responseCode = "204", description = "Partial update", content = @Content),
-            @ApiResponse(responseCode = "404", description = "Not found",
-                    content = {@Content(mediaType = "JSON Value",
-                            examples = @ExampleObject(value = ("Organization ID is not found.")),
+            @ApiResponse(responseCode = "204", description = "Partial update"),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
                             schema = @Schema(implementation = ErrorDetails.class))}),
-            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "JSON Value",
-                    examples = @ExampleObject(value = "Incorrect parameters in JSON Object"))),
-            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content)
+            @ApiResponse(responseCode = "401", description = "Forbidden, Invalid or expired Token",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "403", description = "Unauthorized role",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "404", description = "Not found",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = ErrorDetails.class))}),
 
     })
     void updateOrganization(@Valid @NotNull @PathVariable Long id, @Valid @RequestBody UpdateOrganizationRequest updateOrganizationRequest);
+
     @SecurityRequirement(name = "bearerAuth")
     @Operation(summary = "update organization completely or created new organization", responses = {
-            @ApiResponse(responseCode = "204", description = "Completely update", content = @Content),
-            @ApiResponse(responseCode = "201", description = "Created a new Organization", content = @Content),
-            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "JSON Value",
-                    examples = @ExampleObject(value = "Incorrect parameters in JSON Object"))),
-            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content)
+            @ApiResponse(responseCode = "204", description = "Completely update"),
+            @ApiResponse(responseCode = "201", description = "Created a new Organization"),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "401", description = "Forbidden, Invalid or expired Token",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "403", description = "Unauthorized role",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = ErrorDetails.class))}),
+
     })
     ResponseEntity<Void> updateOrganization(@Valid @NotNull @PathVariable Long id, @Valid @RequestBody OrganizationRequest organizationRequest);
 }
