cefcce48d0c2c136e93d3363792e1ac52944d6f0
jisantillan <javierignaciosantillan.u@gmail.com>
2022-05-18 22:30:39 -0300

    Added UpdateCategoryRequest

---
+++ src/main/java/com/alkemy/ong/ports/input/rs/request/UpdateCategoryRequest.java
@@ -0,0 +1,26 @@
+package com.alkemy.ong.ports.input.rs.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateCategoryRequest {
+
+    @NotBlank
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("image")
+    private String image;
+}
