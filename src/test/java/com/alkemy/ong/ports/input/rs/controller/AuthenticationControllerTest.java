7bf4bda9db16acc5578e0d84d5e2118fb5a192e5
locknet11 <locknet41@protonmail.com>
2022-05-21 15:48:20 -0300

    tests

--- src/test/java/com/alkemy/ong/ports/input/rs/controller/AuthenticationControllerTest.java
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/AuthenticationControllerTest.java
@@ -1,6 +1,8 @@
 package com.alkemy.ong.ports.input.rs.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,10 +20,15 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import com.alkemy.ong.common.exception.handler.GlobalExceptionHandler;
 import com.alkemy.ong.common.security.services.AuthenticationService;
 import com.alkemy.ong.common.util.JsonUtils;
+import com.alkemy.ong.domain.model.User;
 import com.alkemy.ong.domain.usecase.UserService;
 import com.alkemy.ong.ports.input.rs.mapper.AuthenticationControllerMapper;
+import com.alkemy.ong.ports.input.rs.mapper.UserControllerMapper;
 import com.alkemy.ong.ports.input.rs.request.CreateUserRequest;
 import com.alkemy.ong.ports.input.rs.response.RegisterResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 @ExtendWith(MockitoExtension.class)
 class AuthenticationControllerTest {
@@ -31,7 +38,8 @@ class AuthenticationControllerTest {
 	@InjectMocks AuthenticationController controller;
 	@Mock AuthenticationService authService;
 	@Mock UserService userService;
-	@Spy AuthenticationControllerMapper mapper = Mappers.getMapper(AuthenticationControllerMapper.class);
+	@Spy UserControllerMapper userMapper = Mappers.getMapper(UserControllerMapper.class);
+	@Spy AuthenticationControllerMapper authMapper = Mappers.getMapper(AuthenticationControllerMapper.class);
 	
 	@BeforeEach
     void setUp() {
@@ -45,6 +53,12 @@ class AuthenticationControllerTest {
 
 		CreateUserRequest request = CreateUserRequest.builder().email("test@test.com").firstName("Test")
 				.lastName("User").password("testuser1").build();
+		
+		User thisUser = authMapper.createUserRequestToUser(request);
+		thisUser.setId(99L);
+		
+		given(userService.createUser(any(User.class))).willReturn(thisUser);
+		given(authService.singIn(any(), any())).willReturn("jwt");
 		
 		final String content = mockMvc
 				.perform(post("/auth/register").contentType(MediaType.APPLICATION_JSON)
