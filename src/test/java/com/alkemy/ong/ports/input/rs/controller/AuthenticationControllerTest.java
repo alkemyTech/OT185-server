2df10d4d56aca8bbf1cd4c044e1332a4f560059d
Jonathan Manera <manerajona@gmail.com>
2022-05-22 14:01:54 -0300

    Fix UT

--- src/test/java/com/alkemy/ong/ports/input/rs/controller/AuthenticationControllerTest.java
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/AuthenticationControllerTest.java
@@ -1,11 +1,15 @@
 package com.alkemy.ong.ports.input.rs.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.alkemy.ong.common.exception.handler.GlobalExceptionHandler;
+import com.alkemy.ong.common.security.services.AuthenticationService;
+import com.alkemy.ong.common.util.JsonUtils;
+import com.alkemy.ong.domain.model.User;
+import com.alkemy.ong.domain.usecase.UserService;
+import com.alkemy.ong.ports.input.rs.mapper.AuthenticationControllerMapper;
+import com.alkemy.ong.ports.input.rs.request.CreateUserRequest;
+import com.alkemy.ong.ports.input.rs.response.RegisterResponse;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,64 +18,76 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import com.alkemy.ong.common.exception.handler.GlobalExceptionHandler;
-import com.alkemy.ong.common.security.services.AuthenticationService;
-import com.alkemy.ong.common.util.JsonUtils;
-import com.alkemy.ong.domain.model.User;
-import com.alkemy.ong.domain.usecase.UserService;
-import com.alkemy.ong.ports.input.rs.mapper.AuthenticationControllerMapper;
-import com.alkemy.ong.ports.input.rs.mapper.UserControllerMapper;
-import com.alkemy.ong.ports.input.rs.request.CreateUserRequest;
-import com.alkemy.ong.ports.input.rs.response.RegisterResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class AuthenticationControllerTest {
-	
-	private MockMvc mockMvc;
-	
-	@InjectMocks AuthenticationController controller;
-	@Mock AuthenticationService authService;
-	@Mock UserService userService;
-	@Spy UserControllerMapper userMapper = Mappers.getMapper(UserControllerMapper.class);
-	@Spy AuthenticationControllerMapper authMapper = Mappers.getMapper(AuthenticationControllerMapper.class);
-	
-	@BeforeEach
+
+    private MockMvc mockMvc;
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    @InjectMocks
+    AuthenticationController controller;
+    @Mock
+    AuthenticationService authService;
+    @Mock
+    UserService userService;
+    @Spy
+    AuthenticationControllerMapper mapper = Mappers.getMapper(AuthenticationControllerMapper.class);
+
+    @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(controller)
                 .setControllerAdvice(GlobalExceptionHandler.class)
                 .build();
     }
-	
-	@Test
-	void createUser_shouldReturn201() throws Exception {
 
-		CreateUserRequest request = CreateUserRequest.builder().email("test@test.com").firstName("Test")
-				.lastName("User").password("testuser1").build();
-		
-		User thisUser = authMapper.createUserRequestToUser(request);
-		thisUser.setId(99L);
-		
-		given(userService.createUser(any(User.class))).willReturn(thisUser);
-		given(authService.singIn(any(), any())).willReturn("jwt");
-		
-		final String content = mockMvc
-				.perform(post("/auth/register").contentType(MediaType.APPLICATION_JSON)
-						.content(JsonUtils.objectToJson(request)))
-				.andExpect(status().isCreated()).andDo(print()).andReturn().getResponse().getContentAsString();
+    @Test
+    void createUser_shouldReturn201() throws Exception {
 
-		assertThat(content).isNotBlank();
+		String username = "test@test.com";
+		String password = "testuser1";
 
-		RegisterResponse response = JsonUtils.jsonToObject(content, RegisterResponse.class);
+		CreateUserRequest request = CreateUserRequest.builder()
+				.email(username)
+				.firstName("Test")
+                .lastName("User")
+				.password(password)
+				.build();
 
-		assertThat(response.getAuthResponse().getJwt()).isNotBlank();
-		assertThat(response.getUserResponse().getEmail()).isEqualTo(request.getEmail());
+		User user = new User();
+		user.setEmail(username);
 
-	}
+		given(userService.createUser(any(User.class))).willReturn(user);
+		given(authService.singIn(eq(username),eq(password))).willReturn("token");
+
+        final String content = mockMvc
+                .perform(post("/auth/register").contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtils.objectToJson(request)))
+				.andExpect(status().isCreated())
+				.andDo(print()).andReturn()
+				.getResponse()
+				.getContentAsString();
+
+        assertThat(content).isNotBlank();
+
+        RegisterResponse response = OBJECT_MAPPER.readValue(content, RegisterResponse.class);
+
+        assertThat(response.getAuthResponse().getJwt()).isNotBlank();
+        assertThat(response.getUserResponse().getEmail()).isEqualTo(request.getEmail());
+
+    }
 
 }
