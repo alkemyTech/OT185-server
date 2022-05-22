d888d4f8a4a602edf3373f457202d57408367cb1
locknet11 <locknet41@protonmail.com>
2022-05-22 15:29:03 -0300

    fixed unit test for AuthenticationController

--- src/main/java/com/alkemy/ong/ports/input/rs/request/AuthenticationRequest.java
+++ src/main/java/com/alkemy/ong/ports/input/rs/request/AuthenticationRequest.java
@@ -3,12 +3,15 @@ package com.alkemy.ong.ports.input.rs.request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class AuthenticationRequest {
 
--- src/test/java/com/alkemy/ong/ports/input/rs/controller/AuthenticationControllerTest.java
+++ src/test/java/com/alkemy/ong/ports/input/rs/controller/AuthenticationControllerTest.java
@@ -6,9 +6,11 @@ import com.alkemy.ong.common.util.JsonUtils;
 import com.alkemy.ong.domain.model.User;
 import com.alkemy.ong.domain.usecase.UserService;
 import com.alkemy.ong.ports.input.rs.mapper.AuthenticationControllerMapper;
+import com.alkemy.ong.ports.input.rs.request.AuthenticationRequest;
 import com.alkemy.ong.ports.input.rs.request.CreateUserRequest;
+import com.alkemy.ong.ports.input.rs.response.AuthenticationResponse;
 import com.alkemy.ong.ports.input.rs.response.RegisterResponse;
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.alkemy.ong.ports.input.rs.response.UserResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,15 +20,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -88,6 +89,63 @@ class AuthenticationControllerTest {
         assertThat(response.getAuthResponse().getJwt()).isNotBlank();
         assertThat(response.getUserResponse().getEmail()).isEqualTo(request.getEmail());
 
+    }
+    
+    @Test
+    void userLogin_shouldReturn200() throws Exception {
+    	
+    	String email = "test@test.com";
+    	String password = "testuser1";
+    	
+    	AuthenticationRequest request = AuthenticationRequest.builder()
+    			.email(email)
+    			.password(password)
+    			.build();
+    	
+    	given(authService.singIn(eq(email),eq(password))).willReturn("token");
+    	
+    	final String content = mockMvc
+    			.perform(post("/auth/login").contentType(MediaType.APPLICATION_JSON)
+    					.content(JsonUtils.objectToJson(request)))
+    			.andExpect(status().isOk())
+    			.andDo(print())
+    			.andReturn()
+    			.getResponse()
+    			.getContentAsString();
+    	
+    	assertThat(content).isNotBlank();
+    	
+    	AuthenticationResponse response = OBJECT_MAPPER.readValue(content, AuthenticationResponse.class);
+    	
+    	assertThat(response.getJwt()).isEqualTo("token");
+    }
+    
+    @Test
+    void meData_shouldReturn200() throws Exception {
+    	
+    	UserResponse user = new UserResponse();
+    	user.setId(99L);
+    	user.setFirstName("Test");
+    	user.setLastName("User");
+    	user.setEmail("test@test.com");
+    	user.setPhoto("image.com");
+    	
+    	given(mapper.userToUserResponse(any(User.class))).willReturn(user);
+    	
+    	final String content = mockMvc
+    			.perform(get("/auth/me"))
+    			.andExpect(status().isOk())
+    			.andDo(print())
+    			.andReturn()
+    			.getResponse()
+    			.getContentAsString();
+    	
+    	assertThat(content).isNotBlank();
+    	
+    	UserResponse response = OBJECT_MAPPER.readValue(content, UserResponse.class);
+    	
+    	assertThat(response.getEmail()).isEqualTo(user.getEmail());
+    	assertThat(response.getId()).isEqualTo(user.getId());
     }
 
 }
